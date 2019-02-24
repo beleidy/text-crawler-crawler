4d465962f9cd85247e3b1ecc75cde9d7695210d7
Amr Elbeleidy <amr@elbeleidy.me>
2019-02-24 12:16:37 +0000

    initial crawler commit

---
+++ index.js
@@ -0,0 +1,57 @@
+const elasticsearch = require("elasticsearch");
+const HCCrawler = require("headless-chrome-crawler");
+const RedisCache = require("headless-chrome-crawler/cache/redis");
+const uuidv4 = require("uuid/v4");
+
+const REDIS_HOST = "localhost";
+const REDIS_PORT = 6379;
+const ES_PORT = 9200;
+const ES_HOST = `localhost:${ES_PORT}`;
+
+(async function main() {
+  const cache = new RedisCache({
+    host: REDIS_HOST,
+    port: REDIS_PORT
+  });
+
+  // Create ES client
+  const esClient = new elasticsearch.Client({
+    host: ES_HOST,
+    log: "trace"
+  });
+
+  // Check ES is alive
+  try {
+    await esClient.ping({ requestTimeout: 30000 });
+  } catch (err) {
+    console.error("ES error: ", err);
+  }
+
+  const crawler = await HCCrawler.launch({
+    args: ["--disable-web-security", "--no-sandbox"],
+    persistCache: true,
+    cache,
+    preRequest: options => {
+      if (options.url.match(/\.(css|jpg|pdf|docx|js|png|ico)/i)) {
+        console.log(options.url, "--wrong-file-type --skipped");
+        return false;
+      } else {
+        console.log(options.url, "Requesting");
+        return true;
+      }
+    },
+    onSuccess: response => {
+      console.log(response.response.url, `Status ${response.response.status}`);
+      esClient.index({
+        index: "sites",
+        type: "_doc",
+        body: {
+          domain: new URL(response.response.url).hostname,
+          uri: response.response.url,
+          siteText: response.result,
+          lastUpdated: new Date(Date.now()).toISOString()
+        }
+      });
+    }
+  });
+})();
